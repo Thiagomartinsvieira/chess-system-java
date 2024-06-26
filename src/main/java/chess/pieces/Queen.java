f98dab1e1562a7ee6675b2058c13afcbe2f33f42
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2024-06-25 21:13:37 -0300

    feat: special move - castling

--- src/main/java/chess/pieces/Queen.java
+++ src/main/java/chess/pieces/Queen.java
@@ -6,6 +6,7 @@ import chess.ChessPiece;
 import chess.Color;
 
 public class Queen extends ChessPiece {
+
     public Queen(Board board, Color color) {
         super(board, color);
     }
@@ -56,6 +57,16 @@ public class Queen extends ChessPiece {
         while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
             mat[p.getRow()][p.getColumn()] = true;
             p.setRow(p.getRow() + 1);
+        }
+        if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        // nw
+        p.setValues(position.getRow() - 1, position.getColumn() - 1);
+        while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setValues(p.getRow() - 1, p.getColumn() - 1);
         }
         if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
             mat[p.getRow()][p.getColumn()] = true;
